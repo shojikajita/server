e9edadaa69c2075feb167eb5ac1dd09050314db2
hilak <hila.karimov@kaltura.com>
2013-10-06 12:46:12 +0300

    add documentation

--- api_v3/lib/types/enums/KalturaEntryModerationStatus.php
+++ api_v3/lib/types/enums/KalturaEntryModerationStatus.php
@@ -10,4 +10,15 @@ class KalturaEntryModerationStatus extends KalturaEnum
 	const REJECTED = 3;   
 	const FLAGGED_FOR_REVIEW = 5;
 	const AUTO_APPROVED = 6;
+	
+	public static function getDescriptions()
+	{
+		return array(
+			self::PENDING_MODERATION => 'Moderation status given to entries uploaded to a partner account configured to automatically moderate incoming content',
+			self::APPROVED => 'Moderation status given to an entry that is approved for display',
+			self::REJECTED => 'Moderation status given to an entry that is rejected because of inappropriate content',
+			self::FLAGGED_FOR_REVIEW => 'Moderation status signifying that an end user flagged the entry for inappropriate content',
+			self::AUTO_APPROVED => 'Moderation status signifying that the entry was automatically approved for display',
+		);
+	}
 }
